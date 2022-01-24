368d275941dde3a25db2e9eedc5868ce38a7dd95
Marcus Vinicius Menezes dos Santos <marcus.santos1808@hotmail.com>
2022-01-24 15:57:38 -0300

    first commit

---
+++ src/API/Entities/Product.cs
@@ -0,0 +1,12 @@
+namespace API.Entities;
+
+public class Product
+{
+    public Product()
+    {
+        Name = string.Empty;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
