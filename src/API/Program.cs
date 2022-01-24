97a8c599a5de5f2ea56448a140e7d7cc2c52d9fd
Marcus Vinicius Menezes dos Santos <marcus.santos1808@hotmail.com>
2022-01-24 16:19:15 -0300

    feat: Added connString for default db in dev

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,11 +1,21 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Add Swagger services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// EF configuration (DbContexts)
+builder.Services.AddDbContext<StoreContext>(options => {
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
